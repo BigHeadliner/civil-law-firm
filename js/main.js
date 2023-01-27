ac319603a5a6865989dec5f5ee8276cddfff1e77
BigHeadliner <bigheadliner@gmail.com>
2023-01-27 16:55:09 +0200

    everything works good

--- js/main.js
+++ js/main.js
@@ -1,54 +1,54 @@
-$(function () { 
-    
-   $('.header__btn-menu').on('click', function(){
+$(function () {
+
+   $('.header__btn-menu').on('click', function () {
       $('.menu ul').slideToggle()
-   }); 
-    
-   // $('.menu a').on('click', function(){ 
-   //    $('.menu ul').slideToggle()
-   // })  
+   });
+
+   $('.menu a').on('click', function () {
+      $('.menu ul').slideToggle()
+   })
 
    $('.news__slider-inner').slick({
       nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
       prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-      infinite: false 
-     
-   }); 
-    
-   $('.slider__inner').slick({  
+      infinite: false
+
+   });
+
+   $('.slider__inner').slick({
       nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
       prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
       responsive: [
          {
-           breakpoint: 901,
-           settings: {
-            autoplay: true,
-           }
-         }, 
+            breakpoint: 901,
+            settings: {
+               autoplay: true,
+            }
+         },
       ]
-   }) 
-    
+   })
+
    $(".menu a").on("click", function (event) {
       //відміняєм стандартну обробку кліку по ссилці
-    event.preventDefault();
-  
+      event.preventDefault();
+
       // забираєм індифікатор блоку з атрибутом href
-    var id  = $(this).attr('href'),
-  
-      //дізнаємся висоту від початку сторінки до блоку на який зсилається якір
-      top = $(id).offset().top;
-    
+      var id = $(this).attr('href'),
+
+         //дізнаємся висоту від початку сторінки до блоку на який зсилається якір
+         top = $(id).offset().top;
+
       //анімуєм перехід на відстань - top за 1500мс
-    $('body,html').animate({scrollTop: top}, 1500);
-  });
+      $('body,html').animate({ scrollTop: top }, 1500);
+   });
 
    $('select').styler();
 
- 
-    
-   $('.default-btn--form').on('click', function(e){ 
-      e.preventDefault();
-    })   
 
-}); 
- 
+
+   $('.default-btn--form').on('click', function (e) {
+      e.preventDefault();
+   })
+
+});
+
